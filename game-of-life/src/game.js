051f706236f08f65d0e8e052520f4d4bd339cb46
Jared Brown <jaredbrown1@hotmail.com>
2020-10-20 18:48:40 -0700

    rules were updated

--- game-of-life/src/game.js
+++ game-of-life/src/game.js
@@ -25,10 +25,10 @@ How can Conway's "Game of Life" be useful in real life?
 
 const rules = () => {
   alert(`The rules of "The Game of Life":
-        #1- Any live cell with fewer than two live neighbors dies (under-population).
-        #2- Any live cell with three live neighbors live on to the next generation.
-        #3- Any live cell with more than three live neighbors dies (overpopulation).
-        #4- Any dead cell with exactly three live neighbors becomes a live cell (reproduction).`);
+  1. Any live cell with fewer than two live neighbors dies, as if caused by under population.
+  2. Any live cell with two or three live neighbors lives on to the next generation.
+  3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+  4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.`);
 };
 
 class Cell extends React.Component {
